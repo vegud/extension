2b243473e048637105f43337222ecfb456540d95
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2021-03-22 20:42:31 +0100

    Setup vegud extension

--- src/popup/select/pages/SelectSubtitle.vue
+++ src/popup/select/pages/SelectSubtitle.vue
@@ -1,8 +1,10 @@
 <template>
-  <PageLayout :content-transition-name='contentTransitionName' :has-back="videoCount > 1" :back-fn='backFn'>
+  <PageLayout :content-transition-name='contentTransitionName' :has-back='videoCount > 1' :back-fn='backFn'>
     <template #content>
-      <div>
-        select
+      <div v-for='(entry, index) in filtered' :key='index'
+           class='hover:cursor-pointer hover:bg-primary-700 hover:text-on-primary-700'
+           @click="selectSubtitle(entry)" >
+        {{ entry }}
       </div>
     </template>
   </PageLayout>
@@ -10,7 +12,7 @@
 
 <script lang='ts'>
 
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, onMounted, PropType, ref } from 'vue';
 import PageLayout from '@/components/PageLayout.vue';
 import { useInjectStore } from '@/composables/useInjectStore';
 
@@ -28,10 +30,29 @@ export default defineComponent({
   setup() {
     const navigationStore = useInjectStore('navigationStore');
     const videoStore = useInjectStore('videoStore');
+    const selectStore = useInjectStore('selectStore');
+    const url = new URL(window.location.href);
+    const filtered = ref<string[]>([]);
+
+    const prefix = url.hostname === 'www.youtube.com' ? 'yt' : '<unknown>';
+    const videoId = url.searchParams.get('v') ?? '';
+
+    onMounted(async () => {
+      const list = await selectStore.actions.list();
+      filtered.value = list.filter(e => e.startsWith(`${prefix}/${videoId}`));
+    });
+
+
+    console.warn(window.location.hostname === 'www.youtube.com');
+
 
     return {
       videoCount: videoStore.getters.count,
       toSettings: navigationStore.actions.toSettings,
+      filtered,
+      selectSubtitle: (entry) => {
+        console.warn('select'+entry);
+      },
       backFn: (): void => {
         videoStore.actions.removeCurrent();
         navigationStore.actions.toHome();
--- src/popup/select/store/index.ts
+++ src/popup/select/store/index.ts
@@ -7,6 +7,9 @@ export interface SelectState {
 
 export interface SelectStore {
   state: ComputedRef<SelectState>;
+  actions: {
+    list: () => Promise<string[]>
+  }
 }
 
 
@@ -14,6 +17,15 @@ export const init = ({ accessToken }: Pick<UnwrapRef<SelectState>, 'accessToken'
   const state = ref<SelectState>({ accessToken });
 
   return {
-    state: computed(() => state.value)
+    state: computed(() => state.value),
+    actions: {
+      list: async () => {
+        return fetch(`https://5m1ansbaba.execute-api.eu-west-1.amazonaws.com/list`, {
+          headers: {
+            Authorization: `${accessToken}`
+          }
+        }).then(r => r.json());
+      }
+    }
   };
 };
